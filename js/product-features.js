423263b7d3f034120a25d101cfce8b897f0d6ddf
VinizinXpl <mvinicius18072006@gmail.com>
2025-10-16 20:24:05 -0300

    Uns ajustes ai

---
+++ js/product-features.js
@@ -0,0 +1,73 @@
+/* Product Features Database */
+
+const productFeatures = {
+    'Shika Beta': {
+        Player: [
+            'Attack Effects',
+            'Custom Element',
+            'Extra Hit',
+            'God Mode',
+            'Infinite Energy',
+            'Infinite Stamina',
+            'Infinite Wanderer Stamina',
+            'Infinite Phlogiston',
+            'Infinite Nightsoul',
+            'Infinite Mavuika Stamina',
+            'No Clip',
+            'No Skill Cooldown',
+            'No Avatar Change Cooldown',
+            'No Avatar Combat',
+            'No Hide Weapon',
+            'Instant Bow Charge',
+            'No Sprint Cooldown',
+            'No Vehicle Skill Cooldown',
+            'Player Speed'
+        ],
+        World: [
+            'Auto Challenge',
+            'Auto Destroy',
+            'Auto Loot',
+            'Auto Seelie',
+            'Auto Talk',
+            'Skip Cutscenes',
+            'Dumb Enemies',
+            'Permanent Elemental Sight',
+            'Fake Time',
+            'Fast Combine',
+            'Freeze Enemies',
+            'Kill Aura',
+            'Mob Vacuum',
+            'Auto Music Game',
+            'Open Team Inmediately'
+        ],
+        Teleport: [
+            'Chest Teleport',
+            'Map Teleport',
+            'Oculi Teleport',
+            'Quest Teleport'
+        ],
+        Visual: [
+            'ESP',
+            'Camera Zoom',
+            'No Censorship',
+            'FOV Changer',
+            'GM Icon',
+            'Graphics Changer',
+            'Hide Damage',
+            'Hide Reaction',
+            'Hide UI',
+            'No Fog',
+            'No Grass',
+            'No Map Fog',
+            'Paimon Follow',
+            'Profile Changer',
+            'Unlock FPS'
+        ],
+        Misc: [
+            'Instant Exit',
+            'Gacha Url Fetcher'
+        ]
+    }
+};
+
+export default productFeatures;
